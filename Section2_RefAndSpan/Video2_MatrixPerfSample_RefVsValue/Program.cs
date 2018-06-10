20f962886845e3d1a41cb42881abd8795d41c710
Gergely Kalapos <gergo@kalapos.net>
2018-06-10 22:55:12 +0200

    Adding Section2/Video2 samples

---
+++ Section2_RefAndSpan/Video2_MatrixPerfSample_RefVsValue/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Video2_MatrixPerfSample_RefVsValue
+{
+	public struct Matrix
+	{
+		public readonly int i00, i01, i02;
+		public readonly int i10, i11, i12;
+		public readonly int i20, i21, i22;
+
+		public Matrix(int i00, int i01, int i02, int i10, int i11, int i12, int i20, int i21, int i22)
+		{
+			this.i00 = i00;
+			this.i01 = i01;
+			this.i02 = i02;
+
+			this.i10 = i10;
+			this.i11 = i11;
+			this.i12 = i12;
+
+			this.i20 = i20;
+			this.i21 = i21;
+			this.i22 = i22;
+		}
+	}
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<Program>();
+		}
+
+		[Benchmark]
+		public static void RefReturnPerf()
+		{
+			Random rnd = new Random();
+			for (int i = 0; i < 100; i++)
+			{
+				Matrix m1 = new Matrix(N(), N(), N(), N(), N(), N(), N(), N(), N());
+				Matrix m2 = new Matrix(N(), N(), N(), N(), N(), N(), N(), N(), N());
+
+				ref Matrix biggerMatrix = ref GetBiggerByReference(ref m1, ref m2);
+				Debug.WriteLine(biggerMatrix.i00);
+			}
+			
+			int N() => rnd.Next(Int32.MaxValue / 5);
+		}
+
+		[Benchmark]
+		public static void RetByValuePerf()
+		{
+			Random rnd = new Random();
+			for (int i = 0; i < 100; i++)
+			{
+				Matrix m1 = new Matrix(N(), N(), N(), N(), N(), N(), N(), N(), N());
+				Matrix m2 = new Matrix(N(), N(), N(), N(), N(), N(), N(), N(), N());
+
+				Matrix biggerMatrix = GetBiggerByValue(m1, m2);
+				Debug.WriteLine(biggerMatrix.i00);
+			}
+
+			int N() => rnd.Next(Int32.MaxValue / 5);
+		}
+
+
+
+		public static Matrix GetBiggerByValue(Matrix matrix1, Matrix matrix2)
+		{
+			var sum1 = matrix1.i00 + matrix1.i01 + matrix1.i02 +
+						matrix1.i10 + matrix1.i11 + matrix1.i12 +
+						matrix1.i20 + matrix1.i21 + matrix1.i22;
+
+			var sum2 = matrix2.i00 + matrix2.i01 + matrix2.i02 +
+						matrix2.i10 + matrix2.i11 + matrix2.i12 +
+						matrix2.i20 + matrix2.i21 + matrix2.i22;
+
+			if (sum2 > sum1)
+				return matrix2;
+			else
+				return matrix1;
+		}
+
+		public static ref Matrix GetBiggerByReference(ref Matrix matrix1, ref Matrix matrix2)
+		{
+			var sum1 = matrix1.i00 + matrix1.i01 + matrix1.i02 +
+						matrix1.i10 + matrix1.i11 + matrix1.i12 +
+						matrix1.i20 + matrix1.i21 + matrix1.i22;
+
+			var sum2 = matrix2.i00 + matrix2.i01 + matrix2.i02 +
+						matrix2.i10 + matrix2.i11 + matrix2.i12 +
+						matrix2.i20 + matrix2.i21 + matrix2.i22;
+
+			if (sum2 > sum1)
+				return ref matrix2;
+			else
+				return ref matrix1;
+		}
+	}
+}
---
+++ Section2_RefAndSpan/Video2_RefReturAndRefLocal/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Video2_RefReturAndRefLocal
+{
+	class Program
+	{
+		public class SampleClass
+		{
+			private int _intField;
+
+			public int IntProperty { get => _intField; set => _intField = value; }
+
+			public ref int M() => ref _intField;
+		}
+
+		static void Main(string[] args)
+		{
+			MyList l = new MyList();
+			ref int i = ref l.GetRefToSmallestItem();
+			i = 42;
+
+			Console.WriteLine("Hello World!");
+		}
+
+		public static ref int MethodReturningIntByRef(ref int intValue)
+		{
+			//...do something with 'intValue'
+			return ref intValue;
+		}
+
+
+		public static ref object MethodReturningObjByRef(ref object obj)
+		{
+			//...do something with 'obj'
+			return ref obj;
+		}
+		
+		class MyList
+		{
+			private int[] _items = { 3, 4, 7, 1, 4, 6 };
+
+			public ref int GetRefToSmallestItem()
+			{
+				var smallest = _items[0];
+				int index = 0;
+
+				for (int i = 1; i < _items.Length; i++)
+				{
+					if (_items[i] < smallest)
+					{
+						smallest = _items[i];
+						index = i;
+					}
+				}
+
+				return ref _items[index];
+			}
+		}
+	}
+}
