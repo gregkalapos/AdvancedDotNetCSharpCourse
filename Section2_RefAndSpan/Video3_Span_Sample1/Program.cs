5fc191f9fcbacdf93a3836cb824034ea57bcf484
Gergely Kalapos <gergo@kalapos.net>
2018-06-11 18:40:34 +0200

    Adapt code to text

--- Section2_RefAndSpan/Video3_Span_Sample1/Program.cs
+++ Section2_RefAndSpan/Video3_Span_Sample1/Program.cs
@@ -7,7 +7,7 @@ namespace Video3_Span_Sample1
 		static void Main(string[] args)
 		{
 			Span<byte> span = new Span<byte>(new byte[] { 2, 4, 4, 1 });
-			var newSpan = span.Slice(3, 2);
+			var newSpan = span.Slice(1, 3);
 			span[1] = 42;
 		}
 	}
