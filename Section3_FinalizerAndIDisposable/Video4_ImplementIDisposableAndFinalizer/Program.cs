2bfc39546e8c12181a5a6e24d7f90381f12de6ea
Gergely Kalapos <gergo@kalapos.net>
2018-06-17 23:48:58 +0200

    Adding section3/video4 sample (free managed&unmanaged resources)

---
+++ Section3_FinalizerAndIDisposable/Video4_ImplementIDisposableAndFinalizer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Video4_ImplementIDisposableAndFinalizer
+{
+	class Program
+	{
+		class SampleClass: IDisposable
+		{
+			IntPtr intPtr;
+			StreamReader streamReader;
+
+
+			public SampleClass()
+			{
+				intPtr = Marshal.AllocHGlobal(1000000000);
+				streamReader = new StreamReader("sample.txt");
+			}
+
+			protected void Dispose(bool itIsSafeToAlsoFreeManagedObjects)
+			{
+				Marshal.FreeHGlobal(intPtr);
+
+				if (itIsSafeToAlsoFreeManagedObjects)
+				{
+					if (streamReader != null)
+					{
+						streamReader.Dispose();
+						streamReader = null;
+					}
+				}
+			}
+
+			public void Dispose()
+			{
+				Dispose(true);
+				GC.SuppressFinalize(this);
+			}
+
+			~SampleClass()
+			{
+				Dispose(false);
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
