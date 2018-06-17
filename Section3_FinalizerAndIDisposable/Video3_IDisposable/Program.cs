111fa241364b3f9f7ec31af4383c99941189afbd
Gergely Kalapos <gergo@kalapos.net>
2018-06-17 22:45:43 +0200

    Adding section3/video 3 sample - IDisposable usage

---
+++ Section3_FinalizerAndIDisposable/Video3_IDisposable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Video3_Finalizer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (StreamReader streamReader = new StreamReader("sample.txt"))
+			{
+				var str = streamReader.ReadToEnd();
+			}
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
