28c13c398dae5ff591cc9764d0e24dace4cc3c98
Gergely Kalapos <gergo@kalapos.net>
2018-06-17 21:22:50 +0200

    Adding section3, Video1&Video2 samples

---
+++ Section3_FinalizerAndIDisposable/Video1_Finalizer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Video1_Finalizer
+{
+	class SampleClass
+	{
+		~SampleClass()
+		{
+			Program.PrintThreadInfo("Finalizer", Thread.CurrentThread);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PrintThreadInfo("Main", Thread.CurrentThread);
+
+			var sampleInstance = new SampleClass();
+			sampleInstance = null;
+						
+			GC.Collect();
+			Console.ReadKey();
+		}
+
+		public static void PrintThreadInfo(string str, Thread thread)
+		{
+			Console.WriteLine($"{str} Priority: {thread.Priority}");
+			Console.WriteLine($"{str} ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+		}
+	}
+}
---
+++ Section3_FinalizerAndIDisposable/Video2_Finalizer_GC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Video2_Finalizer_GC
+{
+	class SampleClass
+	{
+		public int MyProperty { get; set; }
+		~SampleClass()
+		{
+			long sum = 0;
+			Random rnd = new Random();
+			while (true)
+			{
+				sum += rnd.Next(-2, 2);
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var rnd = new Random();
+			while (true)
+			{
+				var sapleInstance = new SampleClass() { MyProperty = rnd.Next() };
+				Debug.WriteLine(sapleInstance.MyProperty);
+				GC.SuppressFinalize(sapleInstance);
+				GC.ReRegisterForFinalize(sapleInstance);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
