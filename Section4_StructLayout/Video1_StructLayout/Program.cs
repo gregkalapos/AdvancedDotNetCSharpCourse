40661cf1c3d74965d89b65c38735f8cf8f8635b6
Gergely Kalapos <gergo@kalapos.net>
2018-06-18 00:02:04 +0200

    Adding section4 sample code (structlayout)

---
+++ Section4_StructLayout/Video1_StructLayout/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Video1_StructLayout
+{
+	struct Struct1
+	{
+		public byte b1;
+		public double d1;
+		public byte b2;
+		public double d2;
+		public byte b3;
+	}
+
+	struct Struct2
+	{
+		public double d1;
+		public double d2;
+		public byte b1;
+		public byte b2;
+		public byte b3;
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			unsafe
+			{
+				Console.WriteLine("Struct1: " + sizeof(Struct1));
+				Console.WriteLine("Struct2: " + sizeof(Struct2));
+			}
+		}
+	}
+}
---
+++ Section4_StructLayout/Video2_StructLayoutAttribute/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Video2_StructLayoutAttribute
+{
+	class Program
+	{
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		struct Struct1
+		{
+			public byte b1;
+			public double d1;
+			public byte b2;
+			public double d2;
+			public byte b3;
+		}
+
+		class C { }
+
+		[StructLayout(LayoutKind.Explicit)]
+		struct Union
+		{
+			[FieldOffset(0)]
+			public byte b;
+			[FieldOffset(0)]
+			public int i;
+			[FieldOffset(0)]
+			public bool boolean;
+		}
+
+		static void Main(string[] args)
+		{
+			Union u = new Union();
+			u.i = 42;
+			Console.WriteLine(u.boolean);
+			u.boolean = false;
+			int b = u.i;
+
+			unsafe
+			{
+				Console.WriteLine("Union: " + sizeof(Union));
+			}
+		}
+	}
+}
